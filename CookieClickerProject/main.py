0f6981c264e4c9fc1ff966cace669535ec9fe501
utsavhansaria <156498866+droneKop@users.noreply.github.com>
2025-09-19 13:46:39 +0530

    Update main.py
    Code to follow users of an instagram account

--- CookieClickerProject/main.py
+++ CookieClickerProject/main.py
@@ -1,183 +1,105 @@
-from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-import  time
+"""
+This script is implementation of a simple way to increase visibility and boost views of an instagram account.
+It will log on to your instagram account, go to a page whose followers you want to follow and then follow them.
+Refer to in-line comments for info or customizations
+"""
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
-
-url = "https://ozh.github.io/cookieclicker/"
-driver.get(url=url)
-click_cookie = driver.find_element(By.ID, value="bigCookie")
-
-i = 0
-grandmaCount=0
-totalGrandmaPrice = 0
-grandmaLink = '//*[@id="product1"]/div[3]'
-
-framCount = 0
-factoryCount = 0
-mineCount = 0
-shipmentCount = 0
-alchemyCount = 0
-portalCount = 0
-cookieCount =0
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException
+import time
 
 
-print("Starting while loop!")
-while i<1000:
-    click_cookie.click()
-    i+=1
-    grandmaPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[2]/div[3]/span").text.replace(",",""))
-    farmPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[3]/div[3]/span").text.replace(",",""))
-    factoryPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[4]/div[3]/span").text.replace(",",""))
-    minePrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[5]/div[3]/span").text.replace(",",""))
-    shipmentPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[6]/div[3]/span").text.replace(",",""))
-    alchemyPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[7]/div[3]/span").text.replace(",",""))
-    portalPrice = float(driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[8]/div[3]/span").text.replace(",",""))
-    # time.sleep(1)
-    cookieCount = float(driver.find_element(By.ID, value="cookies").text.split(" ")[0].replace(",",""))
-    # print("----xxx------")
+# username = "You can write your username here if you are running this script only locally"
+# Or, you can also store your username as Environment variable and call the same
+# Or, the below line will prompt you to enter your username everytime you run this code
+username =  input("Enter your instagram account username: ")
 
-    if cookieCount > grandmaPrice and grandmaCount<=5:
-        grandmaCount += 1
-        print("Buying Grandma!!")
-        totalGrandmaPrice+=grandmaPrice
-        print(f"Purchased so far:{totalGrandmaPrice}")
-        portalClick = driver.find_element(By.XPATH, value=grandmaLink)
-        # print("element found")
-        portalClick.click()
-        # print("yay Grandma")
-        print(f"new grandma count: {grandmaCount}")
-        time.sleep(0.5)
-    else:
-        pass
+# password  = "You can write your password here if you are running this script only locally"
+# Or, you can also store your password as Environment variable and call the same
+# Or, the below line will prompt you to enter your password everytime you run this code
+password = input("Enter your instagram account username: ")
 
-
-    # tMachinePrice = driver.find_element(By.XPATH, value="")
-    # antimatterPrice = driver.find_element(By.XPATH,
-    #                                     value="")
-    # prismPrice = driver.find_element(By.XPATH,
-    #                                       value="")
-    # if cookieCount > portalPrice:
-
-
-# time.sleep(2)
-# print(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
-# print(cookieCount)
-# print("------")
-# print(f"GrandmaPrice: {driver.find_element(By.XPATH, value="/html/body/div/div[2]/div[12]/div[1]/div[3]/div[2]/div[3]/span").text}")
-# print("########")
-# print(f"Farm Price: {float(farmPrice.text.replace(","  ""))}")
-# print(f"Factory Price: {float(factoryPrice.text.replace("," ,""))}")
-# print("------")
-# print(f"Mine Price: {minePrice.text.replace(",","")}")
-# print("########")
-# print(f"shipmentPrice: {shipmentPrice.text.replace(",","")}")
-# print("------")
-# print(f"alchemyPrice: {alchemyPrice.text.replace(",","")}")
-# print("########")
-# print(f"portalPrice: {portalPrice.text}")
-# print("------")
-# print(f"cookieCount: {cookieCount}")
-# print("########")
-# print(type(float(farmPrice.text.replace(",",""))))
-# print("------")
-# print(type(factoryPrice.text.replace(",","")))
-# print("########")
-# print(type(minePrice.text))
-time.sleep(100)
-driver.close()
-driver.quit()
-
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import time
-# from selenium.webdriver.common.keys import Keys
-#
-#
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach",True)
-# driver = webdriver.Chrome(options=chrome_options) # creating the object
-#
-# # driver.get("https://gem.gov.in/")
-# # driver.get("https://www.amazon.in/Sony-CFI-2008A01X-PlayStation%C2%AE5-Console-slim/dp/B0CY5HVDS2?ref_=ast_sto_dp")
-# # driver.get("https://www.python.org/")
-# # driver.get("https://en.wikipedia.org/wiki/Main_Page")
-# driver.get("https://secure-retreat-92358.herokuapp.com/")
-#
-#
-# # # ------For url number 5 above - Form update------
-# fNameTag = driver.find_element(By.NAME, value="fName")
-# lNameTag = driver.find_element(By.NAME, value="lName")
-# emailTag = driver.find_element(By.NAME, value="email")
-#
-# fNameTag.send_keys("Utsav")
-# lNameTag.send_keys("Hansaria")
-# emailTag.send_keys("jhingalala@ho.ho")
-# emailTag.send_keys(Keys.ENTER)
-#
-#
-#
-
-# # ------For url number 4 above - Wikipedia------
-# searchBar = driver.find_element(By.NAME, value="search")
-# print("-----")
-# searchBar.send_keys("Doland Trump")
-# print("-----")
-# searchBar.send_keys(Keys.ENTER)
-# nameTag = driver.find_element(By.LINK_TEXT, value="Harry Crerar")
-# print(nameTag.text)
-# nameTag.click()
-# burialPlace = driver.find_element(By.LINK_TEXT, value="Beechwood Cemetery")
-# print(burialPlace.text)
-# burialPlace.click()
+# similarAccount  = "You can write the name of the account whose users you want to follow here if you are running this script only locally"
+# Or, you can also store your similarAccount as Environment variable and call the same
+# Or, the below line will prompt you to enter your similarAccount everytime you run this code
+# Last option may be best here if you want to follow users from different accounts everytime
+similarAccount = input("Enter your instagram account username: ")
 
 
 
-# # ------For url number 4 above - Wikipedia------
-# english_article_count = driver.find_element(By.XPATH,value="/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[1]/div/div[3]/ul/li[2]/a[1]")
-# print(english_article_count.text)
-# ------------------------------------------------------------------------
+class InstagramFollower:
+    def __init__(self):
+        # creating a configuration object to customize chrome browser launch
+        chrome_options = webdriver.ChromeOptions()
+        # browser will stay open after script execution is complete
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
 
+    def login(self):
+        # instagram url
+        url = "https://www.instagram.com/"
+        self.driver.get(url)
+        print("Opening Instagram...")
+        self.driver.implicitly_wait(3)
 
+        # userInput = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[1]/div/label/input')
+        userInput = self.driver.find_element(By.XPATH, '// *[ @ id = "loginForm"] / div[1] / div[1] / div / label / input')
+        userInput.send_keys(username)
+        passInput = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[2]/div/label/input')
+        passInput.send_keys(password)
+        logIn = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]')
+        logIn.click()
+        print("Clicked on login...")
+        print("--3 second wait--")
+        time.sleep(3)
+        print("--Another 5 second wait--")
+        time.sleep(5)
+        notNow = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        notNow.click()
+        print(f"Entered account home page of {username}")
+        print("--Another 5 second wait--")
+        time.sleep(5)
 
+    def accountSearch(self):
+        print("Searching for account")
+        newUrl = f"https://www.instagram.com/{similarAccount}/followers"
+        self.driver.get(newUrl)
+        print("Opening account page")
+        print("--6 second wait--")
+        time.sleep(6)
+        modal = self.driver.find_element(By.XPATH, f"//a[@href='/{similarAccount}/followers/']")
+        modal.click()
+        print(f"Entered follower page of {similarAccount}")
+        print("--Another 5 second wait--")
+        time.sleep(5)
 
-# ------For url number 3 above - Python Website------
-# upcomingEventDates = driver.find_elements(By.CSS_SELECTOR, value="ul li time")
-# upcomingEventDesc = driver.find_elements(By.CSS_SELECTOR, value="section div div div ul li a")
-# # for item in upcomingEventDates:
-# #     print(item.text)
-# #
-# # for item in upcomingEventDesc:
-# #     print(item.text)
-# event_dict = {}
-# for i in range(5,len(upcomingEventDates)):
-#     dict_item = {"date":upcomingEventDates[i].text,
-#                      "event":upcomingEventDesc[i].text
-#                      }
-#     event_dict[str(i-5)] = dict_item
-# print(event_dict)
-# ------------------------------------------------------------------------
+    def follow(self):
+        print("Started follow")
+        # all_buttons = self.driver.find_elements(By.LINK_TEXT, value="Follow")
+        # all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value="._aano button")
+        # followClass = " _aswp _aswr _aswu _asw_ _asx2"
+        followButton = "body > div.x1n2onr6.xzkaem6 > div:nth-child(2) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.x15fl9t6.x1yw9sn2.x1evh3fb.x4giqqa.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div.x6nl9eh.x1a5l9x9.x7vuprf.x1mg3h75.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6 > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div:nth-child(3) > div > button"
 
+        all_buttons = self.driver.find_elements(By.CLASS_NAME, value=followButton)
+        print(all_buttons)
+        print("Check your buttons")
+        for button in all_buttons:
+            try:
+                button.click()
+                print("--Followed 1 user--")
+                print("--2 second wait--")
+                time.sleep(2)
+                print("Followed successfully")
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(by=By.XPATH, value="//button[contains(text(), 'Cancel')]")
+                cancel_button.click()
 
-
-# ------For url number 2 above - Amazon Product link------
-# price_rs = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-# print(f"The price is: {price_rs.text}")
-# seller_from_css = driver.find_element(By.ID, value="sellerProfileTriggerId")
-# print(f"The Seller is: {seller_from_css.text}")
-# delivery_xpath1 = driver.find_element(By.XPATH, value='//*[@id="contextualIngressPtLabel_deliveryShortLine"]')
-# delivery_xpath2 = driver.find_element(By.XPATH, value="/html/body/div[2]/div/div/div[5]/div[1]/div[3]/div/div/div/div[1]/div/div/div/form/div/div/div/div/div[4]/div/div[4]/div[10]/div[2]/span/a/div/div/div/span/div")
-# print("------")
-# print(f"Delivery Location 1: {delivery_xpath1.text}")
-# print(f"Delivery Location 2: {delivery_xpath2.text}")
-# print(f"Delivery Location 2: {delivery_xpath2.get_attribute("id")}")
-# # driver.close()
-# # driver.quit()
-# ------------------------------------------------------------------------
-
-
+newBot = InstagramFollower()
+newBot.login()
+newBot.accountSearch()
+newBot.follow()
 
 
